e182ce4a70ae101b367306090a5eabde6a5a322f
SuyashMuley <59868486+SuyashMuley@users.noreply.github.com>
2021-08-05 19:53:06 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9da86f9cbaff7fc1b36cf499922e9521'),
+    },
+  },
+});
